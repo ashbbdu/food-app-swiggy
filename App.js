1797b41b345fcde6274801babe570098474ed7f0
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-11-01 16:54:54 +0530

    Lecture 1

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+    "h1",
+    {
+      title: "heading",
+    },
+    "Namaste React 1"
+  );
+
+  const heading2 = React.createElement(
+    "h2",
+    {
+      title: "heading",
+    },
+    "Namaste React 2"
+  );
+
+  const container = React.createElement("div", {}, [heading, heading2]);
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(container);
